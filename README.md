68cec6ebc158bfc4e19c41f68df531eb59b77c5a
ringosingson <ringosingson@live.com>
2019-05-19 02:25:49 +0530

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# HomeHooks
-Created with CodeSandbox
---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+function App() {
+  const [userQuery, setUserQuery] = useState("");
+  const updateUserQuery = event => {
+    console.log(userQuery);
+    setUserQuery(event.target.value);
+  };
+
+  const handleKeyPress = event => {
+    if (event.key === "Enter") {
+      searchQuery();
+    }
+  };
+
+  const searchQuery = () => {
+    window.open(`https://google.com/search?q=${userQuery}`, "_blank");
+  };
+
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <div className="form">
+        <input
+          value={userQuery}
+          onChange={updateUserQuery}
+          onKeyPress={handleKeyPress}
+        />
+        <button onClick={searchQuery}>Search</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
